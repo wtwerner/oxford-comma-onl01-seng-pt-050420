46cc07cea46375daa0ae0d76a446e53a50c8fc39
Tommy Werner <wtwerner@gmail.com>
2020-05-10 16:59:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array[0]
+  elsif array.count == 2 
+    array.join(" and ")
+  else
+    array[0..-2].join(", ") + ", and #{array[-1]}"
+end
 end
