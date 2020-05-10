e6bdce9e1b3c5d9c3edc748e9470cc16e5a1543b
Tommy Werner <wtwerner@gmail.com>
2020-05-10 16:58:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,7 @@ def oxford_comma(array)
     array[0]
   elsif array.count == 2 
     array.join(" and ")
-  elsif array.count >= 3
-    array[0..-2].join(", ") + ", and #{array[-1]}"
   else
-end
+    array[0..-2].join(", ") + ", and #{array[-1]}"
+  end
 end
