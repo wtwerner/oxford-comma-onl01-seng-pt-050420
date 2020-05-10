7f0fe1078016e083eba362fd5d55f11bfec1c280
Tommy Werner <wtwerner@gmail.com>
2020-05-10 16:53:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
-  if array.count == 2 
+  if array.count == 1
+    "#{array}"
+  elsif array.count == 2 
     array.join(" and ")
   elsif array.count == 3
     array[0,1].join(", ") + ", and #{array[3]}"
